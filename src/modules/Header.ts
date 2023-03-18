ebc1ba98864d705ac6b1cdedc2cb9d448dd070bc
Nicholas Peters <peters.nicholas@gmail.com>
2023-03-18 09:56:01 +0100

    Add login functionality

--- src/modules/Header.ts
+++
@@ -1,9 +0,0 @@
-const app = document.getElementById('app') as HTMLDivElement;
-
-export default function Header() {
-    app.innerHTML = `
-      <header>
-        <h1>Digital Guestbook</h1>
-      </header>
-    `;
-  }
